acb95f32ddbaaf36b4132abd4d37e945dbf0cb56
Marisa Mortellaro <mmortellaro@carthage.edu>
2017-10-06 13:52:41 -0500

    square
    adding function square()

--- lab1.cpp
+++ lab1.cpp
@@ -1,4 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
 main()
 {
     
+}
+int square(int base, int exp)
+{
+    int result;
+    int result = Math.pow(base,exp);
+    return result; 
 }
