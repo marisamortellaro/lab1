8b5b4f8472e83997c20f6f13c62e8caa18136a93
Marisa Mortellaro <mmortellaro@carthage.edu>
2017-10-06 13:59:45 -0500

    tests
    added some code in main() to test power()

--- lab1.cpp
+++ lab1.cpp
@@ -4,7 +4,16 @@
 using namespace std;
 main()
 {
-    
+    int a = 2;
+    int b = 3;
+    int c= power(2,3);
+    if(c==8)
+    {
+        cout << "power() works!"
+    } else
+    {
+        cout<< "Power() does not work"
+    }
 }
 void power(int base, int exp)
 {
